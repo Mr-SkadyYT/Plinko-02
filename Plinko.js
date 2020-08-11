51f1184353fe0275077a58d19f34be0d094c4caa
Saarthak <sasmitadiabagh@gmail.com>
2020-08-11 19:55:59 +0530

    C

---
+++ Plinko.js
@@ -0,0 +1,17 @@
+class Plinko {
+    constructor(x, y,radius) {
+        var options={
+            isStatic:true,
+        }
+        this.body = Bodies.circle(x,y,radius,options);
+        this.radius = radius;
+        World.add(world, this.body);
+    }
+    display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        ellipseMode(CENTER);
+        ellipse(0, 0, this.radius,this.radius);
+        pop();
+    }
+}
--- sketch.js
+++ sketch.js
@@ -14,12 +14,22 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    for (var k = 0;k<=width; k = k+80) {
+        divisions.push(new Division(k, height-divisionHeight/2,10,divisionHeight));
+    }
+    for (var k = 0;k<=width; k = k+80) {
+        plinko.push(new Plinko(k, height-divisionHeight/2,10,divisionHeight));
+    }
 
     ground = new Ground(600,height,1200,20);
 }
 
 function draw(){
-    for var (k = 0;k<=width; k = k+80) {
-        divisions.push(new divisions(k, height-divisionHeight/2,10,divisionHeight));
-    }
+   background(0);
+   Engine.update(engine);
+   for (k = 0;k<divisions.length; k = k+1) {
+    divisions[k].display();
+    plinko[k].display();
 }
+}
+git add "Division.js" "index.html" "sketch.js" "Plinko.js"
